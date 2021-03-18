95f896bf9e319837396e05e5f626662bd33b9288
Steven Moseni <smoseni@hotmail.com>
2021-03-17 22:09:28 -0700

    updated api and view js files

--- server.js
+++ server.js
@@ -3,3 +3,14 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static(__dirname + '/Public'));
+
+app.use(express.urlencoded({extended: true }));
+app.use(express.json())
+
+require('./Routes/api')(app);
+require('./Routes/view')(app);
+
+app.listen(PORT, function() {
+    console.log(`Server is listening on PORT: ${PORT}`);
+  });
