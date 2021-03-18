8237595da6f266d0e80a661f29307af69be31a63
Steven Moseni <smoseni@hotmail.com>
2021-03-17 23:45:17 -0700

    updated view.js

--- Routes/view.js
+++ Routes/view.js
@@ -1,3 +1,15 @@
 const path = require('path');
 
-module.exports = function(app){}
+module.exports = function(app){
+    app.get('/notes', function(app){
+        res.sendFile(path.join(_dirname, '../Public/notes.html'));
+    });
+
+    app.get('/', function(req, res) {
+        res.sendFile(path.join(_dirname, '../public/index.html'));
+    });
+
+    app.get('*', function(req, res) {
+        res.sendFile(path.join(_dirname, '../public/index.html'));
+    });
+}
