4171a01068a2fa50affe0105fb45afe4f1d6ff08
Steven Moseni <smoseni@hotmail.com>
2021-03-17 23:12:56 -0700

    add api post requests

--- Routes/api.js
+++ Routes/api.js
@@ -1,3 +1,12 @@
 const fs = require('fs');
 
-module.exports = function(app) {}
+module.exports = function(app) {
+
+app.get('/api/notes', function(req, res) {
+    fs.readFile('./db/db.json', (err, data) => {
+        if (err) throw err;
+        dbData = JSON.parse(data);
+        res.send(dbData);
+    });
+})
+}
