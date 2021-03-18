b3549929c21ed431e002bc525b255a00ace65391
Steven Moseni <smoseni@hotmail.com>
2021-03-18 00:04:04 -0700

    updated css

--- js/index.js
+++
@@ -1,6 +0,0 @@
-const $noteTitle = $('.note-title');
-const $noteText = $('.note-textarea');
-const $saveNoteBtn = $('.save-note');
-const $newNoteBtn = $('.new-note');
-const $noteList = $('.list-container .list-group');
-
